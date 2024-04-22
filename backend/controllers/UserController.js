e5a9aa1ad6b1ce570acce8a756c11eb3b5acd211
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-22 05:44:38 -0300

    feat: add request to get authenticated user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -60,4 +60,9 @@ const login = async (req, res) => {
   });
 };
 
-module.exports = { register, login };
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+  res.status(200).json(user);
+};
+
+module.exports = { register, login, getCurrentUser };
