aaed14acc80b22f0c4a7f7fc2eab0fa032c7a337
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-22 04:08:31 -0300

    feat: add validations to create a new user

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { register } = require("../controllers/UserController");
 const validate = require("../middleware/handleValidation");
+const { userCreateValidation } = require("../middleware/userValidations");
 
-router.post("/register", validate, register);
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router;
