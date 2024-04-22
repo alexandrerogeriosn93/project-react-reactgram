ed737700a989ec54bc2f57728b836836ba3359ae
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-22 02:15:05 -0300

    feat: add base router configuration

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,12 @@ const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT;
 const app = express();
+const router = require("./routes/Router.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
