a2a71adf3bb0f6274f335d94f952b6a5a23138ec
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-22 10:03:38 -0300

    feat: add request to insert new Photo

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+const { insertPhoto } = require("../controllers/PhotoController");
 const { photoInsertValidation } = require("../middleware/photoValidation");
 const authGuard = require("../middleware/authGuard");
 const validate = require("../middleware/handleValidation");
+const { imageUpload } = require("../middleware/imageUpload");
+
+router.post(
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidation(),
+  validate,
+  insertPhoto
+);
 
 module.exports = router;
