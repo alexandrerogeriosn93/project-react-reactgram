95fa49ba088d35faf2f6de524fb23edef23e9684
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-23 09:52:20 -0300

    feat: config router and init components Home, Login and Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import "./App.css";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
