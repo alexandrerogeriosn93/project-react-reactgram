c63fcecfa36e11829bf060f2a1e042481d3783ab
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-25 09:50:10 -0300

    feat: Navbar to authenticated user

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import {
   BsFillCameraFill,
   BsFillPersonFill,
   BsHouseDoorFill,
   BsSearch,
 } from "react-icons/bs";
-import { Link, NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
+import { useAuth } from "../../hooks/useAuth";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav id="nav">
       <Link to="/">ReactGram</Link>
@@ -17,17 +23,39 @@ const Navbar = () => {
         <input type="text" name="search" id="search" placeholder="Pesquisar" />
       </form>
       <ul id="nav-links">
-        <li>
-          <NavLink to="/">
-            <BsHouseDoorFill />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Entrar</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Registrar</NavLink>
-        </li>
+        {auth ? (
+          <>
+            <li>
+              <NavLink to="/">
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink to="/profile">
+                <BsFillPersonFill />
+              </NavLink>
+            </li>
+            <li>
+              <span>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Registrar</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
