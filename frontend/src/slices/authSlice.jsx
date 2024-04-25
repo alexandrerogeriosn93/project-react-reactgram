549a26c7b143d0ab3c00f0e555b82d8220e50a35
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-25 11:45:22 -0300

    feat: create slice login

--- frontend/src/slices/authSlice.jsx
+++ frontend/src/slices/authSlice.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import authService from "../services/authService";
@@ -56,12 +57,37 @@ export const authSlice = createSlice({
         state.success = true;
         state.error = null;
         state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.user = null;
       });
   },
 });
 
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
+});
+
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  const data = await authService.login(user);
+
+  if (data.errors) {
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
 });
 
 export const { reset } = authSlice.actions;
