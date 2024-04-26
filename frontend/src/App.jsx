10a7e9eff043c02362384cc23c7a50672ec5e054
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-26 01:00:02 -0300

    feat: create component EditProfile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { useAuth } from "./hooks/useAuth";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import EditProfile from "./pages/EditProfile/EditProfile";
 import Home from "./pages/Home/Home";
 
 function App() {
@@ -24,6 +25,10 @@ function App() {
             <Route
               path="/"
               element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/profile"
+              element={auth ? <EditProfile /> : <Navigate to="/login" />}
             />
             <Route
               path="/login"
