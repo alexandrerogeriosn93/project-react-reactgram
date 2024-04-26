752e03b1e5ce07979ba954a6a8bcd63e07f397f5
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-26 17:59:28 -0300

    feat: create User profile slice

--- frontend/src/slices/userSlice.jsx
+++ frontend/src/slices/userSlice.jsx
@@ -1,5 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+import userService from "../services/userService";
+
 const initialState = {
   user: {},
   error: false,
@@ -16,7 +18,30 @@ export const userSlice = createSlice({
       state.message = null;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(profile.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(profile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      });
+  },
 });
+
+export const profile = createAsyncThunk(
+  "user/profile",
+  async (user, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await userService.profile(user, token);
+
+    return data;
+  },
+);
 
 export const { resetMessage } = userSlice.actions;
 export default userSlice.reducer;
