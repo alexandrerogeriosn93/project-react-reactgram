866e1b842abf6dfcbcd738a42bbed85a6827e04b
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-28 12:40:20 -0300

    feat: create form to post Photo

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -13,6 +13,12 @@ const Profile = () => {
   const dispatch = useDispatch();
   const { user, loading } = useSelector((state) => state.user);
   const { user: userAuth } = useSelector((state) => state.auth);
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   useEffect(() => {
     dispatch(getUserDetails(id));
@@ -33,6 +39,24 @@ const Profile = () => {
           <p>{user.bio}</p>
         </div>
       </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={handleSubmit}>
+              <label>
+                <span>Título para a foro</span>
+                <input type="text" placeholder="Insira um título" />
+              </label>
+              <label>
+                <span>Imagem</span>
+                <input type="file" />
+              </label>
+              <input type="submit" value="Postar" />
+            </form>
+          </div>
+        </>
+      )}
     </div>
   );
 };
