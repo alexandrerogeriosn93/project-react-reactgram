ae71ae387595dcc2d5a9d020e0878049b9800d74
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-28 12:24:36 -0300

    feat: return user data to component Profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -4,11 +4,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 
 import Message from "../../components/Message/Message";
+import { getUserDetails } from "../../slices/userSlice";
 import { uploads } from "../../utils/config";
 import "./Profile.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
