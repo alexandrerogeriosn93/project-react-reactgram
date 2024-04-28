44f567b971b829a6f20d7a819b8bdc19b4571e54
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-28 15:35:54 -0300

    feat: add Profile final styles css

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -119,7 +119,13 @@ const Profile = () => {
                   />
                 )}
                 {id === userAuth._id ? (
-                  <p>Actions</p>
+                  <div className="actions">
+                    <Link to={`photos/${photo._id}`}>
+                      <BsFillEyeFill />
+                    </Link>
+                    <BsPencilFill />
+                    <BsXLg />
+                  </div>
                 ) : (
                   <Link className="btn" to={`photos/${photo._id}`}>
                     Ver
