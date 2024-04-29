9bb6580956d44305b710955360e9590822e734e6
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-29 11:15:14 -0300

    feat: create slice to like photo

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -96,6 +96,27 @@ export const photoSlice = createSlice({
         state.success = true;
         state.error = null;
         state.photo = action.payload;
+      })
+      .addCase(like.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        if (state.photo.likes) {
+          state.photo.likes.push(action.payload.userId);
+        }
+
+        state.photos.map((photo) => {
+          if (photo._id === action.payload.photo.photoId) {
+            return photo.likes.push(action.payload.userId);
+          }
+          return photo;
+        });
+        state.message = action.payload.message;
+      })
+      .addCase(like.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
       });
   },
 });
@@ -165,6 +186,17 @@ export const getPhoto = createAsyncThunk(
     return data;
   },
 );
+
+export const like = createAsyncThunk("photo/like", async (id, thunkAPI) => {
+  const token = thunkAPI.getState().auth.user.token;
+  const data = await photoService.like(id, token);
+
+  if (DataTransfer.errors) {
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
+});
 
 export const { resetMessage } = photoSlice.actions;
 export default photoSlice.reducer;
