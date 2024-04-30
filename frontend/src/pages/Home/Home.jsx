cca4d5bf732c0dc2a4fcdf81ad073ec542048551
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-29 22:51:07 -0300

    feat: concluding home page and add home styles css

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -10,12 +10,11 @@ import "./Home.css";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const resetMessage = useResetComponentMessage();
+  const resetMessage = useResetComponentMessage(dispatch);
   const { user } = useSelector((state) => state.auth);
   const { photos, loading } = useSelector((state) => state.photo);
 
-  const handleLike = (e, photo) => {
-    e.preventDefault();
+  const handleLike = (photo) => {
     dispatch(like(photo._id));
     resetMessage();
   };
@@ -28,7 +27,26 @@ const Home = () => {
     return <p>Carregando...</p>;
   }
 
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      {photos &&
+        photos.map((photo) => (
+          <div key={photo._id}>
+            <PhotoItem photo={photo} />
+            <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+            <Link className="btn" to={`/photos/${photo._id}`}>
+              Ver mais
+            </Link>
+          </div>
+        ))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Ainda não há fotos publicadas,{" "}
+          <Link to={`/users/${user.userId}`}>Clique aqui</Link>
+        </h2>
+      )}
+    </div>
+  );
 };
 
 export default Home;
