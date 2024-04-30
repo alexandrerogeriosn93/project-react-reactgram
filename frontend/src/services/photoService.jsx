cd363ab00d3a1103218d9d56149910e31af09c9e
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-04-29 23:42:36 -0300

    feat: create service to search photos by title

--- frontend/src/services/photoService.jsx
+++ frontend/src/services/photoService.jsx
@@ -112,6 +112,20 @@ const getPhotos = async (token) => {
   }
 };
 
+const searchPhotos = async (query, token) => {
+  const config = requestConfig("GET", null, token);
+
+  try {
+    const res = await fetch(`${api}/photos/search?q=${query}`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
@@ -121,6 +135,7 @@ const photoService = {
   like,
   comment,
   getPhotos,
+  searchPhotos,
 };
 
 export default photoService;
